35edbf80076d2c45b113c55d7961c81985749029
Ulathi Ncube <lathincube@gmail.com>
2026-09-23 00:16:31 +0200

    Feat: add styling to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,25 @@
 import styles from "./Footer.module.css"
 import Logo from "../Logo"
+import { Link } from "react-router"
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <Logo />
+      <nav className={styles.navigation}>
+        <ul className={styles.links}>
+          <li className={styles.item}>
+            <Link className={styles.link} to="/blog">
+              Blog
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link className={styles.link} to="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </footer>
   )
 }
