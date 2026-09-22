14950358b85e92c92f5acecbc5e1621873705acd
Ulathi Ncube <lathincube@gmail.com>
2026-09-22 23:07:20 +0200

    Feat: add svg icon to Form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -80,7 +80,24 @@ function ContactForm() {
         <div className={styles.group}>
           <figure className={styles.box}>
             <button className={styles.submit} type="submit">
-              Send Email
+              <span className={styles.text}>Send Email</span>
+              <span className={styles.container}>
+                <svg
+                  className={styles.icon}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <polyline points="13 17 18 12 13 7"></polyline>
+                  <polyline points="6 17 11 12 6 7"></polyline>
+                </svg>
+              </span>
             </button>
           </figure>
         </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,9 @@ function Hero() {
     <section className={styles.hero}>
       <h1 className={styles.title}>Hello, I'm Ulathi</h1>
       <p className={styles.bio}>
-        I'm a FullStack Developer, with React and NodeJS experience. I love all
+        I'm a FullStack Developer, with{" "}
+        <span className={styles.highlight}>React</span> and{" "}
+        <span className={styles.highlight}>NodeJS</span> experience. I love all
         things that have to do with programming, computers and databases. I use
         Postgres a lot, and yes did I say I love programming?
       </p>
